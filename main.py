538490bd98d75dba4deffb9bb0f8599254831597
Anton Shushmentsev <shushmencev97@mail.ru>
2022-02-04 20:03:01 +0700

    Получаю запрос от сервера и печатаю его в консоль

--- main.py
+++ main.py
@@ -3,7 +3,17 @@ import socket
 
 def run():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('localhost', 5000))
+    server_socket.bind(('localhost', 5001))
+    server_socket.listen()
+
+    while True:
+        client_socket, address = server_socket.accept()
+        request = client_socket.recv(1024)
+        print(request.decode())
+        print('\n', address)
+
+        client_socket.sendall('Hello world!!!'.encode())
+        client_socket.close()
 
 
 if __name__ == '__main__':

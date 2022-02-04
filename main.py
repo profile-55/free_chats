f7b99fb1fc7808b1e16adc16a249ae3c18c23a90
Anton Shushmentsev <shushmencev97@mail.ru>
2022-02-04 20:23:54 +0700

    Сервер отправляет "response" браузеру

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
 import socket
 
 
+URLS = {
+    '/': 'hello index',
+    '/blog': 'hello blog'
+}
+
+
+def parse_request(request):
+    parsed = request.split(' ')
+    method, url = parsed[0], parsed[1]
+    return method, url
+
+
+def generate_headers(method, url):
+    if not method == 'GET':
+        return 'HTTP/1.1 405 Method not allowed\n\n', 405
+
+    if not url in URLS:
+        return 'HTTP/1.1 404 Not found\n\n', 404
+
+    return 'HTTP/1.1 200 OK\n\n', 200
+
+
+def generate_response(request):
+    method, url = parse_request(request)
+    headers, code = generate_headers(method, url)
+    return (headers + 'Hello World!!!').encode()
+
+
 def run():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -10,10 +38,12 @@ def run():
     while True:
         client_socket, address = server_socket.accept()
         request = client_socket.recv(1024)
-        print(request.decode('utf-8'))
+        print(request)
         print('\n', address)
 
-        client_socket.sendall('Hello world!!!'.encode())
+        response = generate_response(request.decode('utf-8'))
+
+        client_socket.sendall(response)
         client_socket.close()
 
 
